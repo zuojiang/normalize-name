b25135f2a27a992889e412d6b688d4f72d5af11f
zuojiang <zuojiang.wang@gmail.com>
2018-02-06 08:36:31 +0800

    trim

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ var normalizeName = require('normalize-name')
 normalizeName('normalize-name') // normalize_name
 normalizeName('README.md')  // README_md
 normalizeName('3rd party') // _3rd_party
-normalizeName('a__b___c') // a_b_c
+normalizeName('_a__b___c_') // a_b_c
 ```
 
 ### License
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function (name) {
   name = '' + name
-  name = name.replace(/[^a-zA-Z0-9_]/g, '_').replace(/_{2,}/g, '_')
+  name = name.replace(/[^a-zA-Z0-9_]/g, '_').replace(/_{2,}/g, '_').replace(/^_/, '').replace(/_$/, '')
   if (/^\d/.test(name)) {
     name = '_' + name
   }
